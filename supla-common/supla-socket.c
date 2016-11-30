769dfe6c5468c16dc12f6f29039e281d1359754c
Pawel Kolodziejski <aquadran@users.sourceforge.net>
2016-11-30 15:02:57 +0100

    use only ssl v3 method

--- supla-common/supla-socket.c
+++ supla-common/supla-socket.c
@@ -144,7 +144,7 @@ SSL_CTX* ssocket_initserverctx(void) {
 
 	OpenSSL_add_all_algorithms();
 	SSL_load_error_strings();
-	method = (SSL_METHOD *)SSLv23_server_method();
+	method = (SSL_METHOD *)SSLv3_server_method();
 	ctx = SSL_CTX_new(method);
 
 	if ( ctx == NULL )
@@ -262,7 +262,7 @@ SSL_CTX* ssocket_client_initctx(void) {
 
 	OpenSSL_add_all_algorithms();
 	SSL_load_error_strings();
-	method = (SSL_METHOD *)SSLv23_client_method();
+	method = (SSL_METHOD *)SSLv3_client_method();
 	ctx = SSL_CTX_new(method);
 
 	if ( ctx == NULL )
